10b334459a4a22e7f27764534ad115aa90992445
huyluuu <vxght302@gmail.com>
2024-07-27 19:45:45 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -7,16 +7,47 @@ local Win = library:Evil("Siling Hub","",_G.Logo )
 local Tab1 = Win:CraftTab('Farm/Quest') -- Name
 local Tab4 = Win:CraftTab('Settings') -- Name
 local Page1 = Tab1:CraftPage('Main',1) -- Name,1 or 2
-
+local Page4 = Tab4:CraftPage('Main',1) -- Name,1 or 2
 Page1:Textbox("Auto Farm","",function(v)
     print(v)
 end)
 
-Page1:Toggle('Auto Farm Level',nil,function(a) -- Toggle,Def,callback
+Page1:Toggle('Auto Farm Level',false,function(a) -- Toggle,Def,callback
     print(a)
 end)
 
-Page1:Toggle('Auto Farm Level',nil,function(a) -- Toggle,Def,callback
-    print(a)
+Page4:Toggle('Auto Haki',false,function(b) -- Toggle,Def,callback
+    haki = b
 end)
 
+spawn(function()
+    while wait() do
+    if haki then
+    pcall(function()
+        local args = {
+            [1] = "Aura"
+        }
+        
+        game:GetService("ReplicatedStorage"):WaitForChild("OtherEvent"):WaitForChild("MainEvents"):WaitForChild("Ability"):InvokeServer(unpack(args))
+    end)
+    end
+    end
+    end)
+
+    Page4:Toggle('Auto Ken',false,function(b) -- Toggle,Def,callback
+        ken = b
+    end)
+    
+    spawn(function()
+        while wait() do
+        if ken then
+        pcall(function()
+            local args = {
+                [1] = "Instinct"
+            }
+            
+            game:GetService("ReplicatedStorage"):WaitForChild("OtherEvent"):WaitForChild("MainEvents"):WaitForChild("Ability"):InvokeServer(unpack(args))
+        end)
+        end
+        end
+        end)
