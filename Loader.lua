2c9c48ba313e1aebd684d92f8ee281a39a4f45d9
huyluuu <vxght302@gmail.com>
2024-07-27 19:56:03 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,19 +1,13 @@
-local normal = loadstring(game:HttpGet(('https://raw.githubusercontent.com/huyluuu/UI/main/cac')))()
+local normal = loadstring(game:HttpGet(('https://raw.githubusercontent.com/huyluuu/UI/main/nhindaubuoi')))()
 
 _G.Color = Color3.fromRGB(0, 0, 255) -- Color UI
 _G.Logo = 13990972098 -- ID Logo Your Hub
+
 local Win = library:Evil("Siling Hub","",_G.Logo )
-
 local Tab1 = Win:CraftTab('Farm/Quest') -- Name
-local Tab4 = Win:CraftTab('Settings') -- Name
 local Page1 = Tab1:CraftPage('',1) -- Name,1 or 2
-local Page4 = Tab4:CraftPage('',1) -- Name,1 or 2
 
-Page1:Toggle('Auto Farm Level',false,function(a) -- Toggle,Def,callback
-    print(a)
-end)
-
-Page4:Toggle('Auto Haki',false,function(b) -- Toggle,Def,callback
+Page1:Toggle('Auto Haki',false,function(b) -- Toggle,Def,callback
     haki = b
 end)
 
@@ -31,7 +25,7 @@ spawn(function()
     end
     end)
 
-    Page4:Toggle('Auto Ken',false,function(b) -- Toggle,Def,callback
+    Page1:Toggle('Auto Ken',false,function(b) -- Toggle,Def,callback
         ken = b
     end)
     
