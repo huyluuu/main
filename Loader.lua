18d0cd39b2753c1da41c5d2d1d8090e37756a6a0
huyluuu <vxght302@gmail.com>
2024-07-27 20:09:47 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -10,27 +10,9 @@ local Page = Tab1:CraftPage('Main',1) -- Name,1 or 2
 Page:Toggle('Auto Haki',false,function(b) -- Toggle,Def,callback
     haki = b
 end)
-
-spawn(function()
-    if haki then
-        local args = {
-            [1] = "Aura"
-        }
+if haki then
+local args = {
+    [1] = "Aura"
+}
         
-        game:GetService("ReplicatedStorage"):WaitForChild("OtherEvent"):WaitForChild("MainEvents"):WaitForChild("Ability"):InvokeServer(unpack(args))
-end
-    end)
-
-Page:Toggle('Auto Ken',false,function(b) -- Toggle,Def,callback
-        ken = b
-    end)
-    
-    spawn(function()
-        if ken then
-            local args = {
-                [1] = "Instinct"
-            }
-            
-            game:GetService("ReplicatedStorage"):WaitForChild("OtherEvent"):WaitForChild("MainEvents"):WaitForChild("Ability"):InvokeServer(unpack(args))
-        end
-        end)
+game:GetService("ReplicatedStorage"):WaitForChild("OtherEvent"):WaitForChild("MainEvents"):WaitForChild("Ability"):InvokeServer(unpack(args))
