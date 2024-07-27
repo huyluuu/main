3f52a17b43965c64548543d12d587dd42b90679c
huyluuu <vxght302@gmail.com>
2024-07-27 20:05:48 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -5,40 +5,34 @@ _G.Logo = 13990972098 -- ID Logo Your Hub
 
 local Win = library:Evil("Siling Hub","",_G.Logo )
 local Tab1 = Win:CraftTab('Farm/Quest') -- Name
-local Page = Tab1:CraftPage('',1) -- Name,1 or 2
+local Page = Tab1:CraftPage('Main',1) -- Name,1 or 2
 
-Page:Toggle('Auto Haki',true,function(b) -- Toggle,Def,callback
+Page:Toggle('Auto Haki',false,function(b) -- Toggle,Def,callback
     haki = b
 end)
 
 spawn(function()
     while wait() do
     if haki then
-    pcall(function()
         local args = {
             [1] = "Aura"
         }
         
         game:GetService("ReplicatedStorage"):WaitForChild("OtherEvent"):WaitForChild("MainEvents"):WaitForChild("Ability"):InvokeServer(unpack(args))
-    end)
-    end
     end
     end)
 
-Page:Toggle('Auto Ken',true,function(b) -- Toggle,Def,callback
+Page:Toggle('Auto Ken',false,function(b) -- Toggle,Def,callback
         ken = b
     end)
     
     spawn(function()
         while wait() do
         if ken then
-        pcall(function()
             local args = {
                 [1] = "Instinct"
             }
             
             game:GetService("ReplicatedStorage"):WaitForChild("OtherEvent"):WaitForChild("MainEvents"):WaitForChild("Ability"):InvokeServer(unpack(args))
-        end)
-        end
         end
         end)
