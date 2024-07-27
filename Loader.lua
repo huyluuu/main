2bb25bfb6425db9797a83f81a01bc75a8c9a0728
huyluuu <vxght302@gmail.com>
2024-07-27 20:12:31 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -9,9 +9,11 @@ local Page = Tab1:CraftPage('Main',1) -- Name,1 or 2
 
 Page:Toggle('Auto Haki',false,function(b) -- Toggle,Def,callback
     haki = b
+    spawn(function()
     local args = {
         [1] = "Aura"
     }
             
     game:GetService("ReplicatedStorage"):WaitForChild("OtherEvent"):WaitForChild("MainEvents"):WaitForChild("Ability"):InvokeServer(unpack(args))
+end)
 end)
