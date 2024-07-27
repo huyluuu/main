de69cef33fe4cc1ca8e56c708c154b34ba06ce52
huyluuu <vxght302@gmail.com>
2024-07-27 19:41:22 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -5,12 +5,18 @@ _G.Logo = 13990972098 -- ID Logo Your Hub
 local Win = library:Evil("Siling Hub","",_G.Logo )
 
 local Tab1 = Win:CraftTab('Farm/Quest') -- Name
-local Page = Tab1:CraftPage('Main',1) -- Name,1 or 2
+local Tab4 = Win:CraftTab('Settings') -- Name
+local Page1 = Tab1:CraftPage('Main',1) -- Name,1 or 2
 
-Page:Textbox("Auto Farm","",function(v)
+Page1:Textbox("Auto Farm","",function(v)
     print(v)
 end)
 
-Page:Toggle('Auto Farm Level',nil,function(a) -- Toggle,Def,callback
+Page1:Toggle('Auto Farm Level',nil,function(a) -- Toggle,Def,callback
     print(a)
 end)
+
+Page1:Toggle('Auto Farm Level',nil,function(a) -- Toggle,Def,callback
+    print(a)
+end)
+
